cf23a02cab806d726a57d12522056a7eaaf17433
Aleks Zieliński <alekszielinski01@gmail.com>
2025-11-07 14:14:50 +0100

    p6

--- projekt1.cpp
+++ projekt1.cpp
@@ -10,7 +10,7 @@ bool pierwsza(int x){
 }
 
 int main() {
-    int a, b;
+    int a, b, k;
     cout << "Podaj liczbe calkowita ";
     cin >> a >> b;
     int wyjscie;
@@ -20,7 +20,17 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Pierwsza" << endl;
         cin >> wyjscie;
+        if(wyjscie == 1){
+            cout << "Podaj liczbe " << endl;
+            cin >> k;
+            if(pierwsza(k)){
+                cout << "Liczba " << k << " jest pierwsza" <<endl;
+            } else{
+                cout << "Liczba " << k << " nie jest pierwsza" <<endl;
+            }
+        }
     } while(wyjscie != 0);
     return 0;
 }
