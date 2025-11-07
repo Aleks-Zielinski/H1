4af54e682b170341784109053ad7112f21061983
Aleks Zieliński <alekszielinski01@gmail.com>
2025-11-07 14:11:38 +0100

    p5

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+bool pierwsza(int x){
+    for(int i = 2; i < x; i++){
+        if(x%i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
